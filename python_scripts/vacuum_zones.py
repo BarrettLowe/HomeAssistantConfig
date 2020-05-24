ee445fe65c2198307dcf18b20f2fb688551db431
Barrett Lowe <Barrett.Lowe@gmail.com>
2020-05-23 23:51:23 -0500

    Vacuum automations, ui, and notifications

---
+++ python_scripts/vacuum_zones.py
@@ -0,0 +1,14 @@
+zones = data.get("zones")
+
+try:
+    service_data = {'entity_id':"vacuum.roborock"}
+    service_data["command"] = 'zoned_cleanup'
+    service_data["params"] = {'zone_ids': zones}
+
+    logger.debug("service_data:")
+    logger.debug(service_data)
+
+    hass.services.call("vacuum","send_command", service_data, False)
+    hass.services.call("automation","turn_on", {"entity_id":"automation.vacuum_to_trashcan"})
+except Exception as e:
+    logger.error(e)
