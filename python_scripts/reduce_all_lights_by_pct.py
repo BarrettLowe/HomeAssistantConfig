ccb47eebc1f3c813f5598b42c6a43fce9f755fad
Barrett Lowe <Barrett.Lowe@gmail.com>
2020-06-07 15:04:32 -0500

    Prevent log errors witn non dimmable entities

--- python_scripts/reduce_all_lights_by_pct.py
+++ python_scripts/reduce_all_lights_by_pct.py
@@ -7,16 +7,18 @@ for device in hass.states.all():
     if device.domain == 'light' and device.entity_id != 'light.undercabinet_lights' and device.state == 'on':
         try:
             logger.error(device.attributes['brightness'])
-            brightness = int(int(device.attributes['brightness']) * (pct/100))
-            service_data = {"entity_id":device.entity_id, 
-                            "brightness":brightness}
+            if device.attributes['supported_features'] & 1: #light.SUPPORT_BRIGHTNESS
 
-            if transition: 
-                service_data["transition"] = transition
-            if color_temp:
-                service_data["color_temp"] = color_temp
+                brightness = int(int(device.attributes['brightness']) * (pct/100))
+                service_data = {"entity_id":device.entity_id, 
+                                "brightness":brightness}
 
-            hass.services.call("light", "turn_on", service_data, False)
+                if transition: 
+                    service_data["transition"] = transition
+                if color_temp:
+                    service_data["color_temp"] = color_temp
+
+                hass.services.call("light", "turn_on", service_data, False)
         except Exception as e:
             logger.error("Problem halfing the brightness of {}".format(device.entity_id))
             logger.error(e)
