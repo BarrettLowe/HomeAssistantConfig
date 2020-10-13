2601440652b7757f61f2854a01f72e301eada882
Barrett Lowe <Barrett.Lowe@gmail.com>
2020-10-13 08:18:21 -0500

    update light dimming script

--- python_scripts/reduce_all_lights_by_pct.py
+++ python_scripts/reduce_all_lights_by_pct.py
@@ -9,16 +9,23 @@ for device in hass.states.all():
             logger.error(device.attributes['brightness'])
             if device.attributes['supported_features'] & 1: #light.SUPPORT_BRIGHTNESS
 
+
                 brightness = int(int(device.attributes['brightness']) * (pct/100))
+                if brightness==0:
+                    brightness = 1
                 service_data = {"entity_id":device.entity_id, 
                                 "brightness":brightness}
 
+
                 if transition: 
                     service_data["transition"] = transition
-                if color_temp:
+                if color_temp and device.entity_id != 'light.nursery_dresser': # Leave the nursery dresser at whatever color.
                     service_data["color_temp"] = color_temp
 
-                hass.services.call("light", "turn_on", service_data, False)
+                if not device.attributes['supported_features'] & 32: #light.SUPPORT_TRANSITION
+                    hass.bus.fire("MANUAL_DIM",service_data)
+                else:
+                    hass.services.call("light", "turn_on", service_data, False)
         except Exception as e:
             logger.error("Problem halfing the brightness of {}".format(device.entity_id))
             logger.error(e)
